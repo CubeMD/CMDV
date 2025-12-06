33ce116e86ed786b9f4d0f407a82d72de5f1428e
CubeMD <markelovdp@gmail.com>
2025-12-06 01:02:08 -0500

    1

--- hook/lua/simInit.lua
+++ hook/lua/simInit.lua
@@ -1,6 +1,5 @@
 function BeginSession()
     -- imported for side effects
-    import("/lua/sim/matchstate.lua").Setup()
     import("/lua/sim/markerutilities.lua").Setup()
 
     BeginSessionAI()
@@ -15,6 +14,8 @@ function BeginSession()
     import("/lua/sim/recall.lua").init()
 
     -- other logic at the start of the game --
+    local victoryCondition = import("/lua/sim/victorycondition/VictoryConditionSingleton.lua").GetSingleton()
+    victoryCondition:StartMonitoring()
 
     Sync.EnhanceRestrict = import("/lua/enhancementcommon.lua").GetRestricted()
     Sync.Restrictions = import("/lua/game.lua").GetRestrictions()
